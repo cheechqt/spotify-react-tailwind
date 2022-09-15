46cf47fae68ecab3351dbce443c901b246ef514f
Juzo <fqqwsj@gmail.com>
2022-09-15 22:37:12 +0300

    clear

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/spotify-react-tailwind/">
-      {/* <BrowserRouter> */}
+    {/* <BrowserRouter basename="/spotify-react-tailwind/"> */}
+      <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
