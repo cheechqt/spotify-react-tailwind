92d88571d1a36d28d024e0532d5cf404250ed6da
cheechqt <fqqwsj@gmail.com>
2022-10-15 14:45:07 +0300

    core layout done

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,20 +1,76 @@
 import { Route, Routes } from "react-router-dom";
 
-import TheMain from "../containers/TheMain";
 import TheSignUp from "./TheSignUp";
 import TheLogin from "./TheLogin";
 import TheSearch from "containers/TheSearch";
 import TheLibrary from "containers/TheLibrary";
+import TheHome from "components/TheMain/TheHome/TheHome";
+import { useEffect, useRef } from "react";
+import TheSidebar from "./TheSidebar";
+import TheHeader from "components/TheMain/TheHeader/TheHeader";
+import TheFooter from "components/TheMain/TheFooter/TheFooter";
+import BaseToast from "components/Base/BaseToast";
+import BasePopover from "components/Base/BasePopover/BasePopover";
 
 function App() {
+    const toastRef = useRef();
+    const popoverRef = useRef();
+
+    const contentWrapperRef = useRef();
+    let isScrollingEnabled = true;
+
+    useEffect(() => {
+      const contentWrapper = contentWrapperRef.current;
+
+      contentWrapper.addEventListener("wheel", handleScrolling);
+      return () => contentWrapper.addEventListener("wheel", handleScrolling);
+    });
+
+    const showToast = (message) => {
+      toastRef.current.show(message);
+    };
+
+    const showPopover = (title, description, target, offset) => {
+      popoverRef.current.show(title, description, target, offset);
+    };
+
+    const toggleScrolling = (isEnabled) => {
+      isScrollingEnabled = isEnabled;
+    };
+
+    const handleScrolling = (e) => {
+      if (isScrollingEnabled) return;
+
+      e.preventDefault();
+      e.stopPropagation();
+    };
   return (
-    <Routes>
-      <Route path="/" element={<TheMain />} />
-      <Route path="/signup" element={<TheSignUp />} />
-      <Route path="/login" element={<TheLogin />} />
-      <Route path="/search" element={<TheSearch />} />
-      <Route path="/library" element={<TheLibrary />} />
-    </Routes>
+    <>
+      <div className="flex flex-grow overflow-auto bg-[#121212]">
+        <TheSidebar showPopover={showPopover} />
+        <div className="flex-1 overflow-auto" ref={contentWrapperRef}>
+          <TheHeader />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <TheHome
+                  toggleScrolling={toggleScrolling}
+                  showToast={showToast}
+                />
+              }
+            />
+            <Route path="/signup" element={<TheSignUp />} />
+            <Route path="/login" element={<TheLogin />} />
+            <Route path="/search" element={<TheSearch />} />
+            <Route path="/library" element={<TheLibrary />} />
+          </Routes>
+        </div>
+      </div>
+      <TheFooter />
+      <BaseToast ref={toastRef} />
+      <BasePopover ref={popoverRef} />
+    </>
   );
 }
 
---
+++ src/containers/TheSidebar.js
@@ -0,0 +1,22 @@
+import Footer from "components/TheMain/TheSidebar/Footer/Footer";
+import TheNav from "components/TheMain/TheSidebar/TheNav/TheNav";
+import Overlay from "components/TheMain/TheSidebar/Overlay";
+import BaseLogo from "components/Base/BaseLogo";
+
+function TheSidebar({ showPopover }) {
+  return (
+    <>
+      <aside
+        id="sidebar"
+        className="overflow-hidden top-0 bg-black w-[256px] text-[#b2b2b2] flex lg:sticky fixed flex-col z-30 h-screen lg:h-auto -translate-x-full lg:translate-x-0 target:translate-x-0 transition-transform peer"
+      >
+        <BaseLogo />
+        <TheNav showPopover={showPopover} />
+        <Footer />
+      </aside>
+      <Overlay />
+    </>
+  );
+}
+
+export default TheSidebar;
